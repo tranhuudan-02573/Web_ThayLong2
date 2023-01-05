2bbeff497916e9eafe7466cc295dfd62c24cd5b3
Vo Minh Dat <20130226@st.hcmuaf.edu.vn>
2023-01-06 00:21:16 +0700

    update

--- src/main/java/vn/edu/hcmuaf/fit/constant/Variable.java
+++ src/main/java/vn/edu/hcmuaf/fit/constant/Variable.java
@@ -3,7 +3,8 @@ package vn.edu.hcmuaf.fit.constant;
 public class Variable {
 
     public enum Global {
-        ADMIN, DIFFERENCE, PHONE, USER, JOIN_PHONE, JOIN_ORDER, TG0, JOIN_USER, JOIN_PHONE_CAP, CART, MESSAGE, TYPE, JOIN_PHONE_REVIEW, ORDER
+        ADMIN, DIFFERENCE, PHONE, USER, JOIN_PHONE, JOIN_ORDER, TG0,
+        JOIN_USER, JOIN_PHONE_CAP, CART, MESSAGE, TYPE, JOIN_PHONE_REVIEW, ORDER
     }
 
 }
